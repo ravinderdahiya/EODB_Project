38140e1d6de3fab2b5ed04702aedd4ace8c165b3
nishkant07 <nishkantbeniwal07@gmail.com>
2026-04-16 13:36:27 +0530

    reove folder

--- EODB_LOGIN_AUDIT_15_04_2026/middleware/session.middleware.js
+++
@@ -1,7 +0,0 @@
-exports.verifyLogin = async (req, res, next) => {
-  if (req || req.session || req.session.mobile || req.session.isLogin || req.session.isLoggedIn) {
-    next();
-  } else {
-    res.redirect("/eodb/login");
-  }
-};
